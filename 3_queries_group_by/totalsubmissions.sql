e1d49ef38931d7984249929c50ea2018ac15ede2
Anne Franklin <anmerfrank@gmail.com>
2019-10-15 16:39:14 +0000

    Fixed database issue; all code now running.

--- 3_queries_group_by/totalsubmissions.sql
+++ 3_queries_group_by/totalsubmissions.sql
@@ -1,5 +1,5 @@
-SELECT cohorts.name as cohort, COUNT(assignments_submissions) as total_submissions
-FROM assignments_submissions
+SELECT cohorts.name as cohort, COUNT(assignment_submissions) as total_submissions
+FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
